d4b79e6b23cfbfc2dd18d8c9559d3016d50e477a
Lucas O <oustaloup.lucas@gmail.com>
2025-02-18 16:16:16 +0100

    Initial Commit

---
+++ BandF/BandFApp.swift
@@ -0,0 +1,17 @@
+//
+//  BandFApp.swift
+//  BandF
+//
+//  Created by etudiant on 18/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct BandFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BandFTests/BandFTests.swift
@@ -0,0 +1,17 @@
+//
+//  BandFTests.swift
+//  BandFTests
+//
+//  Created by etudiant on 18/02/2025.
+//
+
+import Testing
+@testable import BandF
+
+struct BandFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
