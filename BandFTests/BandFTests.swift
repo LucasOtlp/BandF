468d96fd7c163130875f5a5354fffe1b13a7ba4d
luc.caslat <148553270+skiaz-p@users.noreply.github.com>
2025-03-10 09:10:25 +0100

    added core files

---
+++ BandFTests/BandFTests.swift
@@ -0,0 +1,17 @@
+//
+//  BandFTests.swift
+//  BandFTests
+//
+//  Created by etudiant on 10/03/2025.
+//
+
+import Testing
+@testable import BandF
+
+struct BandFTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
