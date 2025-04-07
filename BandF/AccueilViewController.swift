90ea8bad99aeab9de1acaccbc27f03f4ba29d5d1
luc.caslat <148553270+skiaz-p@users.noreply.github.com>
2025-04-07 14:30:41 +0200

    Added video to accueil
    DO NOT PLAY THE VIDEO IT WILL CRASH THE APP (it is unfinished)

--- BandF/AccueilViewController.swift
+++ BandF/AccueilViewController.swift
@@ -1,37 +1,101 @@
+//
+//  AccueilPageDetenteViewController.swift
+//  BandF
+//
+//  Created by etudiant on 24/03/2025.
+//
+
 import UIKit
-import WebKit
+import AVFoundation
 
 class AccueilViewController: UIViewController {
 
-    @IBOutlet weak var Video_pres: UIView! // Vue contenant la vidéo
-    var webView: WKWebView!
+    
+    @IBOutlet weak var videoView: UIView!
+    @IBOutlet weak var videoSlider: UISlider!
+    
+    @IBOutlet weak var theoImageView: UIImageView!
+    
+    var player: AVPlayer?
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        // Création de la WebView
-        webView = WKWebView(frame: .zero)
-        webView.translatesAutoresizingMaskIntoConstraints = false
-        Video_pres.addSubview(webView)
-
-        // Ajout de contraintes pour que la WebView remplisse Video_pres
-        NSLayoutConstraint.activate([
-            webView.topAnchor.constraint(equalTo: Video_pres.topAnchor),
-            webView.bottomAnchor.constraint(equalTo: Video_pres.bottomAnchor),
-            webView.leadingAnchor.constraint(equalTo: Video_pres.leadingAnchor),
-            webView.trailingAnchor.constraint(equalTo: Video_pres.trailingAnchor)
-        ])
-
-        // Charger la vidéo avec HTML pour qu'elle s'affiche correctement
-        let videoID = "KLImqL0kZeU" // Remplace par l’ID de ta vidéo YouTube
-        let html = """
-        <!DOCTYPE html>
-        <html>
-        <body style="margin:0;padding:0;overflow:hidden;">
-            <iframe width="100%" height="100%" src="https://www.youtube.com/embed/\(videoID)?playsinline=1" frameborder="0" allowfullscreen></iframe>
-        </body>
-        </html>
-        """
-        webView.loadHTMLString(html, baseURL: nil)
+        showVideo()
+        // Do any additional setup after loading the view.
     }
+    
+    
+    @IBAction func ChangementSurSlider(_ sender: UISlider) {
+        let seconds = Double(sender.value)
+        let targetTime = CMTime(seconds: seconds, preferredTimescale: 600)
+        player?.seek(to: targetTime)
+    }
+    
+    
+    @IBAction func tapSurPlay(_ sender: Any) {
+        player?.play()
+    }
+    
+    
+    @IBAction func tapSurPause(_ sender: Any) {
+        player?.pause()
+    }
+    
+    
+    @IBAction func tapSurRevoir(_ sender: Any) {
+        player?.seek(to: .zero)
+        player?.play()
+    }
+    
+    
+    func addPeriodicTimeObserver() {
+        guard let player = player else { return }
+        // ✅ Attendre que l’item soit prêt avant de lire la durée
+        if let duration = player.currentItem?.asset.duration {
+            let seconds = CMTimeGetSeconds(duration)
+            videoSlider.maximumValue = Float(seconds)
+            videoSlider.minimumValue = 0
+        }
+
+        let interval = CMTime(seconds: 0.5, preferredTimescale: CMTimeScale(NSEC_PER_SEC))
+        player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { [weak self] time in
+            let currentSeconds = CMTimeGetSeconds(time)
+            self?.videoSlider.value = Float(currentSeconds)
+        }
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+
+        // Animation de disparition de l'image
+        UIView.animate(withDuration: 2.0, animations: {
+            self.theoImageView.alpha = 0.0  // Rendre l'image complètement transparente
+        })
+    }
+    
+    func showVideo() {
+        if let path = Bundle.main.path(forResource: "fortbattlepass", ofType: "mp4") {
+            let url = URL(fileURLWithPath: path)
+            player = AVPlayer(url: url)
+            
+            let playerLayer = AVPlayerLayer(player: player)
+            playerLayer.frame = videoView.bounds
+            playerLayer.videoGravity = .resizeAspectFill
+            videoView.layer.addSublayer(playerLayer)
+            
+            // ✅ Ajouter l’observateur ici, une fois le player créé
+            addPeriodicTimeObserver()
+        }
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
 }
