6710bda6f11a2bea8a3414007651d25a72c51bd0
LucasOtlp <oustaloup.lucas@gmail.com>
2025-05-02 17:53:12 +0200

    Ajout de la playlist de détente

--- BandF/PodcastsDetenteViewController.swift
+++ BandF/PodcastsDetenteViewController.swift
@@ -4,16 +4,28 @@
 //
 //  Created by etudiant on 08/04/2025.
 //
-
+import WebKit
 import UIKit
 
 class PodcastsDetenteViewController: UIViewController {
-
+    
+    var youtubeWK: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        youtubeWK = WKWebView(frame: view.bounds)
+        youtubeWK.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+                view.addSubview(youtubeWK)
+        
+        if let url = URL(string: "https://www.youtube.com/playlist?list=PL6NdkXsPL07Lsw96-sKeCMezkn0LuZ8mi") {
+            let request = URLRequest(url: url)
+            youtubeWK.load(request)
+        }
 
         // Do any additional setup after loading the view.
     }
+    
     
 
     /*
