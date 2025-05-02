137a8030580bce4a98aae19911cd1f969e2c8e05
LucasOtlp <oustaloup.lucas@gmail.com>
2025-05-02 16:12:47 +0200

    ajout de l'interface sur la page exercice de repisration

--- BandF/ExercicesRespirationViewController.swift
+++ BandF/ExercicesRespirationViewController.swift
@@ -9,12 +9,45 @@ import UIKit
 
 class ExercicesRespirationViewController: UIViewController {
 
+    
+    @IBOutlet weak var CercleUIView: UIView!
+    @IBOutlet weak var respirationLabel: UILabel!
+    
+    let duration: TimeInterval = 4.0
+    var isInhaling = true
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        CercleUIView.layer.cornerRadius = CercleUIView.frame.size.width / 2
+        CercleUIView.backgroundColor = .systemBlue
+        
+        animationRespiration()
         // Do any additional setup after loading the view.
     }
     
+    
+    @IBAction func tapSurCommencer(_ sender: Any) {
+        
+    }
+    
+    
+    func animationRespiration() {
+            UIView.animate(withDuration: duration, delay: 0, options: [.allowUserInteraction, .curveEaseInOut], animations: {
+                if self.isInhaling {
+                    self.CercleUIView.transform = CGAffineTransform(scaleX: 1.5, y: 1.5)
+                    self.respirationLabel.text = "Inspire"
+                } else {
+                    self.CercleUIView.transform = CGAffineTransform.identity
+                    self.respirationLabel.text = "Expire"
+                }
+            }) { _ in
+                self.isInhaling.toggle()
+                DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                    self.animationRespiration()
+                }
+            }
+        }
 
     /*
     // MARK: - Navigation
